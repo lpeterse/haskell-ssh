1f6bd0d15394e0752bd2691cd6ba6c67b7e5f9a6
Lars Petersen <info@lars-petersen.net>
2017-12-16 22:08:45 +0100

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,55 @@
 module Main where
 
-import Lib
+import           Control.Exception              (bracket)
+import           Control.Monad                  (forever)
+import qualified Crypto.PubKey.Curve25519       as DH
+import qualified Data.Binary.Get                as B
+import qualified Data.ByteString                as BS
+import qualified Data.ByteString.Lazy           as LBS
+import           Data.Monoid
+import           Network.SSH
+import qualified System.Socket                  as S
+import qualified System.Socket.Family.Inet6     as S
+import qualified System.Socket.Protocol.Default as S
+import qualified System.Socket.Type.Stream      as S
+
+import qualified Crypto.PubKey.Curve25519       as Curve25519
+import qualified Crypto.PubKey.Ed25519          as Ed25519
 
 main :: IO ()
-main = someFunc
+main = bracket open close accept
+  where
+    open = S.socket :: IO (S.Socket S.Inet6 S.Stream S.Default)
+    close = S.close
+    accept s = do
+      S.setSocketOption s (S.ReuseAddress True)
+      S.setSocketOption s (S.V6Only False)
+      S.bind s (S.SocketAddressInet6 S.inet6Any 8080 0 0)
+      S.listen s 5
+      forever $ bracket (S.accept s) (S.close . fst) serve
+    serve (s,addr) = do
+      serverSecretKey <- Ed25519.generateSecretKey
+      serverPublicKey <- pure $ Ed25519.toPublic serverSecretKey
+      ephemeralSecretKey <- Curve25519.generateSecretKey
+      ephemeralPublicKey <- pure $ Curve25519.toPublic ephemeralSecretKey
+
+      bs <- S.receive s 4096 S.msgNoSignal
+      print $ B.runGet versionParser (LBS.fromStrict bs)
+      S.sendAllBuilder s 4096 versionBuilder S.msgNoSignal
+      bs <- S.receive s 32000 S.msgNoSignal
+      print $ B.runGet (packetParser kexMsgParser) (LBS.fromStrict bs)
+      S.sendAllBuilder s 4096 (kexMsgBuilder kexMsg) S.msgNoSignal
+      bs <- S.receive s 32000 S.msgNoSignal
+      let pubkey = B.runGet (dhKeyExchangeInitParser) (LBS.fromStrict bs)
+      print pubkey
+      let signature = Ed25519.sign serverSecretKey serverPublicKey (mempty :: BS.ByteString)
+      let reply = KexReply {
+          serverPublicHostKey      = serverPublicKey
+        , serverPublicEphemeralKey = ephemeralPublicKey
+        , exchangeHashSignature    = signature
+        }
+      print reply
+
+      S.sendAllBuilder s 4096 (kexReplyBuilder reply) S.msgNoSignal
+      bs <- S.receive s 32000 S.msgNoSignal
+      print bs
--- src/Network/SSH.hs
+++ src/Network/SSH.hs
@@ -1,26 +1,233 @@
+{-# LANGUAGE BangPatterns      #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Network.SSH where
 
-import qualified Data.Binary.Get as B
+import           Control.Monad           (void, when)
+import qualified Data.Binary.Get         as B
+import qualified Data.ByteString         as BS
+import qualified Data.ByteString.Builder as BS
+import qualified Data.List               as L
+import           Data.Monoid
+import           Data.Word
+import qualified Crypto.PubKey.Curve25519 as DH
+import qualified Crypto.PubKey.Curve25519 as Curve25519
+import qualified Crypto.PubKey.Ed25519 as Ed25519
+import qualified Crypto.Error as DH
+import qualified Data.ByteArray as BA
 
-data Packet
-  = Packet
-  { packetLength :: Word32
-  , paddingLength :: Word8
+data KexMsg
+  = KexMsg
+  { cookie                                  :: BS.ByteString
+  , key_algorithms                          :: [BS.ByteString]
+  , server_host_key_algorithms              :: [BS.ByteString]
+  , encryption_algorithms_client_to_server  :: [BS.ByteString]
+  , encryption_algorithms_server_to_client  :: [BS.ByteString]
+  , mac_algorithms_client_to_server         :: [BS.ByteString]
+  , mac_algorithms_server_to_client         :: [BS.ByteString]
+  , compression_algorithms_client_to_server :: [BS.ByteString]
+  , compression_algorithms_server_to_client :: [BS.ByteString]
+  , languages_client_to_server              :: [BS.ByteString]
+  , languages_server_to_client              :: [BS.ByteString]
+  , first_kex_packet_follows                :: Bool
+  } deriving (Eq, Ord, Show)
 
-clientVersionParser :: B.Get B.ByteString
-clientVersionParser = do
+kexMsg :: KexMsg
+kexMsg = KexMsg
+  { cookie
+  = "\155=\ACK\150\169p\164\v\t\245\223\224\EOT\233\200\SO"
+  , key_algorithms
+  = [ "curve25519-sha256@libssh.org" ]
+  , server_host_key_algorithms
+  = [ "ecdsa-sha2-nistp256-cert-v01@openssh.com" ]
+  , encryption_algorithms_client_to_server
+  = [ "chacha20-poly1305@openssh.com" ]
+  , encryption_algorithms_server_to_client
+  = [ "chacha20-poly1305@openssh.com" ]
+  , mac_algorithms_client_to_server
+  = [ "umac-64-etm@openssh.com" ]
+  , mac_algorithms_server_to_client
+  = [ "umac-64-etm@openssh.com" ]
+  , compression_algorithms_client_to_server
+  = [ "none" ]
+  , compression_algorithms_server_to_client
+  = [ "none" ]
+  , languages_client_to_server
+  = []
+  , languages_server_to_client
+  = []
+  , first_kex_packet_follows
+  = False
+  }
+
+versionParser :: B.Get BS.ByteString
+versionParser = do
   magic <- B.getWord64be
   if magic /= 0x5353482d322e302d -- "SSH-2.0-"
     then stop
     else untilCRLF 0 []
   where
-    stop
-      = fail "syntax error"
+    stop = fail ""
     untilCRLF !i !xs
       = if i >= 255
         then stop
         else B.getWord8 >>= \case
           0x0d -> B.getWord8 >>= \case
-            0x0a -> pure $ B.pack (reverse xs)
+            0x0a -> pure $ BS.pack (reverse xs)
             _    -> stop
           x -> untilCRLF (i+1) (x:xs)
+
+versionBuilder :: BS.Builder
+versionBuilder =
+  version <> BS.int16BE 0x0d0a
+  where
+    version = "SSH-2.0-hssh_0.1"
+
+packetParser :: B.Get a -> B.Get a
+packetParser parser = do
+  packetLen <- fromIntegral . min maxPacketSize <$> B.getWord32be
+  B.isolate packetLen $ do
+    paddingLen <- fromIntegral <$> B.getWord8
+    x <- parser
+    B.skip paddingLen
+    pure x
+
+kexMsgParser :: B.Get KexMsg
+kexMsgParser = do
+  void $ B.getWord8
+  kex <- KexMsg
+    <$> B.getByteString 16
+    <*> nameList
+    <*> nameList
+    <*> nameList
+    <*> nameList
+    <*> nameList
+    <*> nameList
+    <*> nameList
+    <*> nameList
+    <*> nameList
+    <*> nameList
+    <*> ( B.getWord8 >>= \case { 0x00 -> pure False; _ -> pure True } )
+  void $ B.getWord32be -- reserved for future extensions
+  pure kex
+  where
+    nameList = do
+      n <- fromIntegral . min maxPacketSize <$> B.getWord32be -- avoid undefined conversion
+      BS.split 0x2c <$> B.getByteString n
+
+kexMsgBuilder :: KexMsg -> BS.Builder
+kexMsgBuilder msg = mconcat
+  [ BS.word32BE (fromIntegral $ 1 + payloadLen + paddingLen)
+  , BS.word8    (fromIntegral paddingLen)
+  , BS.word8    0x14
+  , BS.byteString (cookie msg)
+  , f (key_algorithms msg)
+  , f (server_host_key_algorithms msg)
+  , f (encryption_algorithms_client_to_server msg)
+  , f (encryption_algorithms_server_to_client msg)
+  , f (mac_algorithms_client_to_server msg)
+  , f (mac_algorithms_server_to_client msg)
+  , f (compression_algorithms_client_to_server msg)
+  , f (compression_algorithms_server_to_client msg)
+  , f (languages_client_to_server msg)
+  , f (languages_server_to_client msg)
+  , BS.word8 $ if first_kex_packet_follows msg then 0x01 else 0x00
+  , BS.word32BE 0x00000000
+  , BS.byteString (BS.replicate paddingLen 0)
+  ]
+  where
+    f xs = BS.word32BE (fromIntegral $ g xs)
+        <> mconcat (BS.byteString <$> L.intersperse "," xs)
+    g [] = 0
+    g xs = sum (BS.length <$> xs) + length xs - 1
+    payloadLen =
+      1 + 16 + 40 + 1 + 4
+      + g (key_algorithms msg)
+      + g (server_host_key_algorithms msg)
+      + g (encryption_algorithms_client_to_server msg)
+      + g (encryption_algorithms_server_to_client msg)
+      + g (mac_algorithms_client_to_server msg)
+      + g (mac_algorithms_server_to_client msg)
+      + g (compression_algorithms_client_to_server msg)
+      + g (compression_algorithms_server_to_client msg)
+      + g (languages_client_to_server msg)
+      + g (languages_server_to_client msg)
+    paddingLen = 16 - (4 + 1 + payloadLen) `mod` 8
+
+data DiffieHellmanGroupExchangeRequest
+  = DiffieHellmanGroupExchangeRequest
+  { dhGexMin :: Word32
+  , dhGexN   :: Word32
+  , dhGexMax :: Word32
+  } deriving (Show)
+
+data DiffieHellmanGroupExchangeGroup
+  = DiffieHelmannGroupExchangeGroup
+  {
+  }
+
+data KexReply
+  = KexReply
+  { serverPublicHostKey      :: Ed25519.PublicKey
+  , serverPublicEphemeralKey :: Curve25519.PublicKey
+  , exchangeHashSignature    :: Ed25519.Signature
+  } deriving (Show)
+
+kexReplyBuilder :: KexReply -> BS.Builder
+kexReplyBuilder reply = mconcat
+  [ BS.word8      31 -- message type
+  , BS.word32BE   51 -- host key len
+  , BS.byteString "ssh-ed25519"
+  , BS.word32BE   32 -- host key data len
+  , BS.byteString $ BS.pack $ BA.unpack (serverPublicHostKey reply)
+  , BS.word32BE   32 -- ephemeral key len
+  , BS.byteString $ BS.pack $ BA.unpack (serverPublicEphemeralKey reply)
+  ]
+
+dhGroupExchangeRequestParser :: B.Get DiffieHellmanGroupExchangeRequest
+dhGroupExchangeRequestParser =
+  packetParser $ do
+    B.getWord8 >>= \w8-> when (w8 /= 34) (fail "expected SSH_MSG_KEY_DH_GEX_REQUEST")
+    DiffieHellmanGroupExchangeRequest
+      <$> B.getWord32be
+      <*> B.getWord32be
+      <*> B.getWord32be
+
+dhKeyExchangeInitParser :: B.Get DH.PublicKey
+dhKeyExchangeInitParser =
+  packetParser $ do
+    msg <- B.getWord8
+    when (msg /= 30) (fail "expected SSH_MSG_KEX_ECDH_INIT")
+    keySize <- B.getWord32be
+    when (keySize /= 32) (fail "expected key size to be 32 bytes")
+    bs <- B.getByteString 32
+    case DH.publicKey bs of
+      DH.CryptoPassed a -> pure a
+      DH.CryptoFailed e -> fail (show e)
+
+{--
+dhGroupExchangeGroupBuilder :: DH.Params -> BS.Builder
+dhGroupExchangeGroupBuilder (DH.Params p g n) = mconcat
+  [ BS.word32BE   $ fromIntegral packetLen
+  , BS.word8      $ fromIntegral paddingLen
+  , BS.word8        31
+  , pBuilder
+  , qBuilder
+  , BS.byteString $ BS.replicate paddingLen 0
+  ]
+  where
+    packetLen  = 1 + payloadLen + paddingLen
+    payloadLen = undefined
+    paddingLen = 16 - (4 + 1 + payloadLen) `mod` 8
+    (pLen, pBuilder) = mpintLenBuilder p (0, mempty)
+    (qLen, qBuilder) = mpintLenBuilder 0 (0, mempty)
+--}
+
+mpintLenBuilder :: Integer -> (Int, BS.Builder) -> (Int, BS.Builder)
+mpingLenBuilder 0 x = x
+mpintLenBuilder i (!len, !bld) = mpintLenBuilder q (len + 4, BS.word32BE (fromIntegral r) <> bld)
+  where
+    (q,r) = i `quotRem` 0x0100000000
+
+maxPacketSize :: Word32
+maxPacketSize = 32767
