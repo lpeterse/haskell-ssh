80dfc30acb0f7970811866ee6c63231d024e24c6
Lars Petersen <info@lars-petersen.net>
2018-01-09 23:07:25 +0100

    WIP

--- app/Foobar.hs
+++ app/Foobar.hs
@@ -43,28 +43,36 @@ main = runInputT $ fix $ \proceed->
   getInputLine "foobar $ " >>= \case
     Nothing -> pure ()
     Just ['a'] -> putDocLine (show [1..]) >> putDocLine "HERE" >> proceed
+    Just ['b'] -> liftIO (threadDelay 2000000) >> putDocLine "DONE" >> proceed
     Just ss -> putDocLine (show ss) >> proceed
 
 runInputT :: InputT IO a -> IO a
 runInputT (InputT r) = do
   interrupt <- newEmptyTMVarIO
-  withRawMode $
-    withHookedSignals $ \waitSignal->
-      withAsync (action interrupt) $ \actionT-> fix $ \proceed-> do
-        let waitAction = Just <$> waitSTM actionT
-        let waitInterrupt = waitSignal >> tryPutTMVar interrupt '\ETX' >>= \case
-              True -> pure Nothing
-              False -> throwSTM UserInterrupt
-        atomically (waitAction `orElse` waitInterrupt) >>= \case
-          Just a  -> pure a
-          Nothing -> proceed
+  withoutEcho $
+    withRawMode $
+      withHookedSignals $ \waitSignal->
+        withAsync (action interrupt) $ \actionT-> fix $ \proceed-> do
+          let waitAction = Just <$> waitSTM actionT
+          let waitInterrupt = waitSignal >> tryPutTMVar interrupt '\ETX' >>= \case
+                True -> pure Nothing
+                False -> throwSTM UserInterrupt
+          atomically (waitAction `orElse` waitInterrupt) >>= \case
+            Just a  -> pure a
+            Nothing -> proceed
   where
+
     withHookedSignals :: (STM () -> IO a) -> IO a
     withHookedSignals action = do
       sig <- newTVarIO False
       bracket
         (flip (installHandler sigINT) Nothing  $ Catch $ atomically $ writeTVar sig True)
         (flip (installHandler sigINT) Nothing) $ const $ action (readTVar sig >>= check >> writeTVar sig False)
+
+    withoutEcho :: IO a -> IO a
+    withoutEcho = bracket
+      (hGetEcho stdin >>= \x-> hSetEcho stdin False >> pure x)
+      (hSetEcho stdin) . const
 
     withRawMode :: IO a -> IO a
     withRawMode = bracket
@@ -82,14 +90,27 @@ runInputT (InputT r) = do
 getInputLine :: MonadIO m => P.Doc -> InputT m (Maybe String)
 getInputLine prompt = do
   putDoc $ show prompt
-  c <- InputT $ do
-    st <- ask
-    liftIO $ withAsync (isGetChar st) $ \t->
-      atomically $ waitSTM t `orElse` takeTMVar (isInterrupt st)
-  case c of
-    '\ETX' -> putDoc "\r\n" >> getInputLine prompt
-    '\EOT' -> pure Nothing
-    _      -> pure (Just [c])
+  mxs <- getLine []
+  putDoc "\r\n"
+  pure mxs
+  where
+    getChar = InputT $ do
+      st <- ask
+      liftIO $ withAsync (isGetChar st) $ \t->
+        atomically $ waitSTM t `orElse` takeTMVar (isInterrupt st)
+
+    getLine acc = getChar >>= \case
+      '\ETX' -> putDoc "\r\n" >> getInputLine prompt
+      '\EOT' -> pure Nothing
+      '\n'   -> pure $ Just (reverse acc)
+      '\DEL' -> case acc of
+        []   -> getLine acc
+        x:xs -> do
+          InputT $ liftIO $ cursorBackward 1 >> putChar ' ' >> cursorBackward 1 >> hFlush stdout
+          getLine $! drop 1 acc
+      c      -> do
+        InputT $ liftIO $ putChar c >> hFlush stdout
+        getLine $! c : acc
 
 putDoc :: MonadIO m => String -> InputT m ()
 putDoc doc = InputT $ do
