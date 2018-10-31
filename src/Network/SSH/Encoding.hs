0b98dd353561f33e5dd42113d5cb233837e6d702
Lars Petersen <info@lars-petersen.net>
2018-10-31 17:25:02 +0100

    Added INLINEABLE pragmas

--- src/Network/SSH/Encoding.hs
+++ src/Network/SSH/Encoding.hs
@@ -34,66 +34,85 @@ putExitCode :: B.Builder b => ExitCode -> b
 putExitCode = \case
     ExitSuccess -> B.word32BE 0
     ExitFailure x -> B.word32BE (fromIntegral x)
+{-# INLINEABLE putExitCode #-}
 
 getExitCode :: Get ExitCode
 getExitCode = getWord32 >>= \case
     0 -> pure ExitSuccess
     x -> pure (ExitFailure $ fromIntegral x)
+{-# INLINEABLE getExitCode #-}
 
 getFramed :: Get a -> Get a
 getFramed g = do
     w <- getWord32
     G.isolate (fromIntegral w) g
+{-# INLINEABLE getFramed #-}
 
 putWord8 :: B.Builder b => Word8 -> b
 putWord8 = B.word8
+{-# INLINEABLE putWord8 #-}
 
 getWord8 :: Get Word8
 getWord8 = G.getWord8
+{-# INLINEABLE getWord8 #-}
 
 expectWord8 :: Word8 -> Get ()
 expectWord8 i = do
     i' <- getWord8
     when (i /= i') (fail mempty)
+{-# INLINEABLE expectWord8 #-}
 
 getWord32 :: Get Word32
 getWord32 = G.getWord32be
+{-# INLINEABLE getWord32 #-}
 
 putBytes :: B.Builder b => BA.ByteArrayAccess ba => ba -> b
 putBytes = B.byteArray
+{-# INLINEABLE putBytes #-}
 
 getBytes :: BA.ByteArray ba => Word32 -> Get ba
 getBytes i = BA.convert <$> G.getByteString (fromIntegral i)
+{-# INLINEABLE getBytes #-}
 
 lenByteString :: BS.ByteString -> Word32
 lenByteString = fromIntegral . BA.length
+{-# INLINEABLE lenByteString #-}
 
 putByteString :: B.Builder b => BS.ByteString -> b
 putByteString = B.byteString
+{-# INLINEABLE putByteString #-}
 
 getByteString :: Word32 -> Get BS.ByteString
 getByteString = G.getByteString . fromIntegral
+{-# INLINEABLE getByteString #-}
 
 getRemainingByteString :: Get BS.ByteString
 getRemainingByteString = G.remaining >>= G.getBytes
+{-# INLINEABLE getRemainingByteString #-}
 
 putString :: (B.Builder b, BA.ByteArrayAccess ba) => ba -> b
 putString ba = B.word32BE (fromIntegral $ BA.length ba) <> putBytes ba
+{-# INLINEABLE putString #-}
 
 putShortString :: B.Builder b => SBS.ShortByteString -> b
 putShortString bs = B.word32BE (fromIntegral $ SBS.length bs) <> B.shortByteString bs
+{-# INLINEABLE putShortString #-}
 
 getShortString :: Get SBS.ShortByteString
 getShortString = SBS.toShort <$> getString
+{-# INLINEABLE getShortString #-}
 
 getString :: BA.ByteArray ba => Get ba
 getString = getWord32 >>= getBytes
+{-# INLINEABLE getString #-}
 
 getName :: Get Name
 getName = Name <$> getShortString
+{-# INLINEABLE getName #-}
 
 putName :: B.Builder b => Name -> b
 putName (Name n) = putShortString n
+{-# INLINEABLE putName #-}
 
 putBool :: B.Builder b => Bool -> b
 putBool False = putWord8 0
