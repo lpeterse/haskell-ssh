0ee34d71ac4df8033ac3ae3a631a53d2a77af0ba
Lars Petersen <info@lars-petersen.net>
2019-01-07 19:09:28 +0100

    Adds socket connection handling for client

--- app/hssh-client/Main.hs
+++ app/hssh-client/Main.hs
@@ -4,14 +4,17 @@
 module Main where
 
 import           Control.Concurrent             ( threadDelay )
+import           Control.Concurrent.Async
 import           Control.Exception              ( bracket
                                                 , bracketOnError
+                                                , catch
                                                 , handle
                                                 , throwIO
                                                 )
 import           Control.Monad.STM
 import qualified Data.ByteArray                 as BA
 import qualified Data.ByteString                as BS
+import           Data.List.NonEmpty             ( NonEmpty (..) )
 import           Data.Default
 import qualified System.Socket                  as S
 import qualified System.Socket.Family.Inet6     as S
@@ -23,7 +26,10 @@ import           Network.SSH
 import           Network.SSH.Client
 
 main :: IO ()
-main = do
+main = withConnection def "google.com" "22" undefined
+
+main2 :: IO ()
+main2 = do
     ai <- getAddressInfo
     bracket open close $ \stream -> do
         let config = def
@@ -56,6 +62,33 @@ main = do
                 atomically exit >>= print
             threadDelay 1000000
             pure ()
+
+withConnection :: Config -> BS.ByteString -> BS.ByteString -> (Connection -> IO a) -> IO ()
+withConnection config host port handler = do
+    (a:as) <- getAddressInfo
+    connectAny (S.socketAddress a :| fmap S.socketAddress as)
+    print "ASHDKAHS"
+    where
+        connectAny :: NonEmpty (S.SocketAddress S.Inet6) -> IO (S.Socket S.Inet6 S.Stream S.Default)
+        connectAny (a:|[]) = connectWithTimeout a
+        connectAny (a:|as) = connectWithTimeout a `catch` \e -> case as of
+            (b:bs) -> connectAny (b:|bs)
+            _      -> throwIO (e :: S.SocketException)
+
+        connectWithTimeout :: S.SocketAddress S.Inet6 -> IO (S.Socket S.Inet6 S.Stream S.Default)
+        connectWithTimeout a = bracketOnError
+            (S.socket :: IO (S.Socket S.Inet6 S.Stream S.Default))
+            (S.close) $ \s -> race
+                (threadDelay (connectionTimeout * 1000) >> throwIO S.eTimedOut) 
+                (S.connect s a) >> pure s
+
+        getAddressInfo :: IO [S.AddressInfo S.Inet6 S.Stream S.Default]
+        getAddressInfo = S.getAddressInfo
+            (Just host) (Just port)
+            (S.aiAddressConfig <> S.aiV4Mapped <> S.aiAll)
+
+        connectionTimeout :: Int
+        connectionTimeout = 10000
 
 -------------------------------------------------------------------------------
 -- Instances for use with the socket library
